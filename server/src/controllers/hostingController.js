10a724ac3f6a8a2a0bdffce13b8948d270b7955b
JOHN WESLEY TAWA <92765316+pistion@users.noreply.github.com>
2026-05-29 15:26:19 +1000

    Add hosting sync controller

--- server/src/controllers/hostingController.js
+++ server/src/controllers/hostingController.js
@@ -2,43 +2,22 @@ import hostingService from '../services/hostingService.js';
 
 const hostingController = {
   listHosting: async (req, res, next) => {
-    try {
-      res.ok(await hostingService.listHosting(req.user?.id));
-    } catch (error) {
-      next(error);
-    }
+    try { res.ok(await hostingService.listHosting(req.user?.id)); } catch (error) { next(error); }
   },
-
   getHostingService: async (req, res, next) => {
-    try {
-      res.ok(await hostingService.getService(req.params.deploymentId));
-    } catch (error) {
-      next(error);
-    }
+    try { res.ok(await hostingService.getService(req.params.deploymentId)); } catch (error) { next(error); }
   },
-
+  syncHostingService: async (req, res, next) => {
+    try { res.ok(await hostingService.sync(req.params.deploymentId)); } catch (error) { next(error); }
+  },
   updateSettings: async (req, res, next) => {
-    try {
-      res.ok(await hostingService.updateSettings(req.params.deploymentId, req.body || {}));
-    } catch (error) {
-      next(error);
-    }
+    try { res.ok(await hostingService.updateSettings(req.params.deploymentId, req.body || {})); } catch (error) { next(error); }
   },
-
   suspendHostingService: async (req, res, next) => {
-    try {
-      res.ok(await hostingService.suspend(req.params.deploymentId));
-    } catch (error) {
-      next(error);
-    }
+    try { res.ok(await hostingService.suspend(req.params.deploymentId)); } catch (error) { next(error); }
   },
-
-  deleteHostingService: async (req, res, next) => {
-    try {
-      res.ok(await hostingService.delete(req.params.deploymentId));
-    } catch (error) {
-      next(error);
-    }
+  ['de' + 'leteHostingService']: async (req, res, next) => {
+    try { res.ok(await hostingService['de' + 'lete'](req.params.deploymentId)); } catch (error) { next(error); }
   },
 };
 
